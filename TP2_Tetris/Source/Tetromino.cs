48f405d0826ee7a002049e24a3b8346fb55bf6b7
nguyhuy <nguyhuy@pc5309e.lan.esiee.fr>
2017-10-23 16:22:11 +0200

    step 2 added

---
+++ TP2_Tetris/Source/Tetromino.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source
+{
+    public class Tetromino
+    {
+        public String firstOrientation;
+        public String secondOrientation;
+        public String thirdOrientation;
+        public String fourthOrientation;
+
+        public Tetromino(String firstOrientation, String secondOrientation, String thirdOrientation, String fourthOrientation)
+        {
+            this.firstOrientation = firstOrientation;
+            this.secondOrientation = secondOrientation;
+            this.thirdOrientation = thirdOrientation;
+            this.fourthOrientation = fourthOrientation;
+        }
+
+        public Tetromino RotateRight()
+        {
+            return new Tetromino(secondOrientation, thirdOrientation, fourthOrientation, firstOrientation);
+        }
+
+        public Tetromino RotateLeft()
+        {
+            return new Tetromino(fourthOrientation, firstOrientation, secondOrientation, thirdOrientation);
+
+        }
+
+        public override String ToString()
+        {
+            return this.firstOrientation;
+        }
+    }
+}
